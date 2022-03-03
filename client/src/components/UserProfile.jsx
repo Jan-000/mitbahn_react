6431029048a9c4c0a32a6ad46175ce89371c9244
Jan Gorecki <jasiekgorecki@gmail.com>
2022-03-03 13:51:09 +0100

    renaming input fields for the train search

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { AuthProviderWrapper } from '../context/auth'
+// import React from 'react'
+// import { AuthProviderWrapper } from '../context/auth'
 
-export default function UserProfile() {
-  return (
-    <div>UserProfile
-   {/* {console.log(user)} */}
-        <div>{user.name} </div>
-    {/* {.user} */}
+// export default function UserProfile() {
+//   return (
+//     <div>UserProfile
+//    {/* {console.log(user)} */}
+//         <div>{user.name} </div>
+//     {/* {.user} */}
 
-    <button id='profile-edit' >EditProfile</button>
-    <button id='profile.delete' >Delete Profile</button>
-    </div>
-  )
-}
+//     <button id='profile-edit' >EditProfile</button>
+//     <button id='profile.delete' >Delete Profile</button>
+//     </div>
+//   )
+// }
