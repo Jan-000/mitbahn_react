43814c1e19f8354c1aeb1c326ae5242ae7c0c0ee
Jan Gorecki <jasiekgorecki@gmail.com>
2022-03-08 22:13:41 +0100

    added logged-in-as feature

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,16 +4,21 @@ import React, { useContext } from 'react'
 import { AuthContext } from "../context/auth"
 
 
-export default function Home() {
-	const {user} = useContext(AuthContext);
-	return (<>
-		<p>You are logged as : 
-        
-		</p>
-		<GoToSearch />
-		{/* {user.email} */}
-		
-		</>
-	)
-}
 
+export default function Home() {
+
+	const {user}  = useContext(AuthContext)
+
+
+
+	return (
+	<> {user ?  <p>You are logged as:  {user.email}
+		</p> : <p>You are not logged in</p>
+		}
+		</>
+
+
+
+	//	<GoToSearch />
+)
+}
