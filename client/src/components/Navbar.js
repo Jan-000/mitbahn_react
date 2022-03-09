ea9c31f901e02ca805d166d08eaf89f25eb751b8
Vicra2501 <Raphaelsonntag@icloud.com>
2022-03-09 17:25:50 +0100

    css first commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,39 +1,35 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { AuthContext } from '../context/auth'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth";
 
 export default function Navbar() {
-
-	const { isLoggedIn, user, logoutUser } = useContext(AuthContext)
+	const { isLoggedIn, user, logoutUser } = useContext(AuthContext);
 
 	return (
-		<nav>
-			<Link to='/'>
-				<button>Home</button>
+		<nav className="nav">
+			<Link to="/">
+				<button className="btn">Home</button>
 			</Link>
-			{isLoggedIn ?
-				(
-					<>
-						<Link to='/groups'>
-							<button>Rides</button>
-						</Link>
-						<button onClick={logoutUser}>Logout</button>
-						<Link to='/userProfile'>
-							<button>Profile</button>
-						</Link>
-					</>
-				) : (
-					<>
-						<Link to='/signup'>
-							<button>Signup</button>
-						</Link>
-						<Link to='/login'>
-							<button>Login</button>
-						</Link>
-
-					</>
-				)}
-			<p>this is navbar.js component</p>
+			{isLoggedIn ? (
+				<>
+					<Link to="/groups">
+						<button>Rides</button>
+					</Link>
+					<button onClick={logoutUser}>Logout</button>
+					<Link to="/userProfile">
+						<button>Profile</button>
+					</Link>
+				</>
+			) : (
+				<>
+					<Link to="/signup">
+						<button>Signup</button>
+					</Link>
+					<Link to="/login">
+						<button>Login</button>
+					</Link>
+				</>
+			)}
 		</nav>
-	)
+	);
 }
