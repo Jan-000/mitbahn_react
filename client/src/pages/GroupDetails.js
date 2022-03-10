22e4579a1521c964976c88ccc65b2fc5885e658a
Vicra2501 <Raphaelsonntag@icloud.com>
2022-03-10 20:43:03 +0100

    css fifth

--- client/src/pages/GroupDetails.js
+++ client/src/pages/GroupDetails.js
@@ -15,90 +15,92 @@ export default function GroupDetails() {
 	let editButtonValidation = false
 	const [group, setGroup] = useState(null);
 	const [message, setMessage] = useState('');
-	const [chat, setChat] = useState (null)
+	const [chat, setChat] = useState(null)
 
 	const storedToken = localStorage.getItem('authToken')
-	const {user} = useContext(AuthContext)
-	const navigate =useNavigate()
+	const { user } = useContext(AuthContext)
+	const navigate = useNavigate()
 
-	const author= user.name
-	
-	if (group){
+	const author = user.name
 
-		for (let i=0; i<group.guests.length; i++){
-	
-		if (group.guests[i]._id === user._id ){
-				joinButtonValidation = false}
-			}}
+	if (group) {
 
-	if (group?.owner === user._id){ joinButtonValidation = false }
-	if (group?.guests.length >= 5){ joinButtonValidation = false }
+		for (let i = 0; i < group.guests.length; i++) {
 
-//conditions for edit button display
+			if (group.guests[i]._id === user._id) {
+				joinButtonValidation = false
+			}
+		}
+	}
 
-	if (group?.owner === user._id){ editButtonValidation = true}
-	
-	
-	
-	
-	
+	if (group?.owner === user._id) { joinButtonValidation = false }
+	if (group?.guests.length >= 5) { joinButtonValidation = false }
+
+	//conditions for edit button display
+
+	if (group?.owner === user._id) { editButtonValidation = true }
+
+
+
+
+
 	const joinGroup = () => {
 		console.log(storedToken)
 		axios.put(`/api/groups/joingroup/${id}`, { user }, { headers: { Authorization: `Bearer ${storedToken}` } })
 			.then(response => {
 
 				setGroup(response.data);
-				joinButtonValidation=false
+				joinButtonValidation = false
 				navigate(`/groups`)
 			})
 			.catch(err => console.log(err))
-			
+
 	}
-   
-	const leaveGroup = () =>{
-		console.log ('clicked leave')
+
+	const leaveGroup = () => {
+		console.log('clicked leave')
 		axios.put(`/api/groups/leavegroup/${id}`, { user }, { headers: { Authorization: `Bearer ${storedToken}` } })
 			.then(response => {
 				setGroup(response.data)
 				joinButtonValidation = true
 				navigate("/groups")
 			})
-			
+
 			.catch(err => console.log(err))
 	}
 
 	const handleSubmit = e => {
 		e.preventDefault()
-		if (group.chat===false){
-			axios.post('/api/groups/initialiseMessage', {  id}, { headers: { Authorization: `Bearer ${storedToken}` } })
-			.then(response => {
-				console.log('chat initialised')
-			})
-			.catch(err => console.log(err))
-			
-		} 
-		
-		axios.post('/api/groups/addMessage', { message,  author, id}, { headers: { Authorization: `Bearer ${storedToken}` } })
+		if (group.chat === false) {
+			axios.post('/api/groups/initialiseMessage', { id }, { headers: { Authorization: `Bearer ${storedToken}` } })
+				.then(response => {
+					console.log('chat initialised')
+				})
+				.catch(err => console.log(err))
+
+		}
+
+		axios.post('/api/groups/addMessage', { message, author, id }, { headers: { Authorization: `Bearer ${storedToken}` } })
 			.then(response => {
 				setMessage('')
-				setChat(response.data)	
+				setChat(response.data)
 			})
 			.catch(err => console.log(err))
-		
-			
+
+
 
 	}
 
 	const handleMessage = e => {
 		setMessage(e.target.value)
 	}
-	
+
 	const getChat = () => {
 		axios.get(`/api/groups/getMessages/${id}`, { headers: { Authorization: `Bearer ${storedToken}` } })
-			.then(response =>{
+			.then(response => {
 				setChat(response.data)
-		})
-		.catch(err => console.log(err))
+			})
+			.catch(err => console.log(err))
 	}
 
 
@@ -113,60 +115,69 @@ export default function GroupDetails() {
 	}, [])
 
 
-console.log("this is group.guests", group?.guests)
+	console.log("this is group.guests", group?.guests)
 
-//conditions for join button display
-	
+	//conditions for join button display
+
 
 	return (
 		<>
-			{ group === null ? <div>Loading ...</div> :
+			{group === null ? <div>Loading ...</div> :
 				<>
-					{console.log(group)}
-					<h1>GroupDetails</h1>
-					<p>start station: {group.startStation}</p>
-					<p>end station : {group.endStation}</p>
-					<p>date : {group.date}</p>
-					<p>You're travelling with :</p>
-					<p>current price is: {group.prices[group.numOfGuests]}</p>
-					{group.guests.map(guest=>{ 
-						return(
-							<p>{guest.name}</p>
-						)
-					 })}
-					<p>here is page GroupDetails.js and {user._id}, {group.owner}</p>
+					<div className='group-details'>
+						{console.log(group)}
+						<h1>GroupDetails</h1>
+						<p>start station: {group.startStation}</p>
+						<p>end station : {group.endStation}</p>
+						<p>date : {group.date}</p>
+						<p>You're travelling with :</p>
+						<p>current price is: {group.prices[group.numOfGuests]}</p>
+						{group.guests.map(guest => {
+							return (
+								<p>{guest.name}</p>
+							)
+						})}
+						<p>here is page GroupDetails.js and {user._id}, {group.owner}</p>
+					</div>
 				</>
 			}
 
 			{<>
-					{ editButtonValidation  &&  <Link to={`/groups/edit/${group._id}`}>
-                        <button>Edit this group</button>
-                    	</Link>}
+				{editButtonValidation && <Link to={`/groups/edit/${group._id}`}>
+					<button className='groupdetails-btn'>Edit this group</button>
+				</Link>}
 
-					{ joinButtonValidation  && <button onClick={joinGroup}>Join this group</button>
-					}
-					{!joinButtonValidation && <button onClick={leaveGroup}>Leave this group</button>}
+				{joinButtonValidation && <button className='groupdetails-btn' onClick={joinGroup}>Join this group</button>
+				}
+				{!joinButtonValidation && <button className='groupdetails-btn' onClick={leaveGroup}>Leave this group</button>}
 
-			
-			<h3>Message Board</h3>
-			
-		    
-			{chat === null ? <div>Loading ...</div> :
-				<>
-					{chat.messages.map(chatMessage => <ChatCard key={chatMessage._id} {...chatMessage} />) }
- 			</>
-			}
+				<h3 className='message-title'>Message Board</h3>
 
-			
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="message">Your message</label>
-				<br></br>
-				<textarea value={message} onChange={handleMessage} id="message" name="message" rows="4" cols="50" 
-				placeholder="use this field for additional information: meeting point, schedule, questions">
-  				</textarea>
-				<br></br>
-				<button id='message-btn' type="submit">Submit</button>
-			</form>
+
+				{chat === null ? <div>Loading ...</div> :
+					<>
+
+						<div className='group-details'>
+							
+						{chat.messages.map(
+						chatMessage => <ChatCard key=
+						{chatMessage._id}
+						 {...chatMessage} />)
+						}
+						</div>
+							
+					</>
+				}
+
+				<form onSubmit={handleSubmit}>
+					<label htmlFor="message">Your message</label>
+					<br></br>
+					<textarea value={message} onChange={handleMessage} id="message" name="message" rows="4" cols="50"
+						placeholder="use this field for additional information: meeting point, schedule, questions">
+					</textarea>
+					<br></br>
+					<button id='message-btn' type="submit">Submit</button>
+				</form>
 
 
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,10 +9,6 @@ export default function Home() {
 
 
 	return (
-<<<<<<< HEAD
-
-=======
->>>>>>> d7c7018dfd6381d823a3b3b6cac86d2599fee34a
 	<p>This is Sparta and also a redeployment!</p>
 
 	)
