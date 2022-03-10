52cf6a205b8fa048ae45cd8d24282625c2c10531
Jan Gorecki <jasiekgorecki@gmail.com>
2022-03-10 18:39:43 +0100

    usual save

--- client/src/components/LoggedUserInfo.js
+++ client/src/components/LoggedUserInfo.js
@@ -12,7 +12,7 @@ export default function Home() {
 
 
 	return (
-	<> {user ?  <p>You are logged as:  {user.email}
+	<> {user ?  <p>You are logged as:  {user.email} and this is redeployment nr 4
 		</p> : <p>You are not logged in</p>
 		}
 		</>
